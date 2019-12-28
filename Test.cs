4301be928f16b9a0f702959f852f2a55c83b4430
RedlMark <33761903+MARKOCA2006@users.noreply.github.com>
2019-12-28 22:37:05 +0100

    Add files via upload

---
+++ Test.cs
@@ -0,0 +1,24 @@
+﻿using MathSharp;
+using System;
+
+namespace MathSharpTest
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            //Print out PI
+            Console.WriteLine("Pi as a float: " + Consts.Pi);
+            Console.WriteLine("Pi as a decimal: " + Consts.PiAsDecimal);
+            Console.WriteLine("Pi as a double: " + Consts.PiAsDouble);
+
+            //Print out E
+            Console.WriteLine("E as a float: " + Consts.e);
+            Console.WriteLine("E as a decimal: " + Consts.eAsDecimal);
+            Console.WriteLine("E as a double: " + Consts.eAsDouble);
+
+            //Stops the project
+            Console.ReadKey();
+        }
+    }
+}
