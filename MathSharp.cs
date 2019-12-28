09eff08ea0601af5d174c67b14221c29757c8d97
RedlMark <33761903+MARKOCA2006@users.noreply.github.com>
2019-12-28 22:07:21 +0100

    Create MathSharp.cs

---
+++ MathSharp.cs
@@ -0,0 +1,32 @@
+namespace MathSharp
+{
+    public class Consts
+    {
+
+        //CONSTS
+
+        // PI
+
+        // Returns the value of PI as a float
+        public static float Pi = (float)3.141592653589793238462643383279502884197169399375105820974944592307816406;
+
+        // Returns the value of PI as a double
+        public static double PiAsDouble = (double)3.141592653589793238462643383279502884197169399375105820974944592307816406;
+
+        // Returns the value of PI as a decimal
+        public static decimal PiAsDecimal = (decimal)3.141592653589793238462643383279502884197169399375105820974944592307816406;
+
+
+
+        // e
+
+        // Returns the value of E as a float
+        public static float e = (float)2.7182818284590452353602874713526624977572470;
+
+        // Returns the value of E as a float
+        public static double eAsDouble = (double)2.7182818284590452353602874713526624977572470;
+
+        // Returns the value of E as a decimal
+        public static decimal eAsDecimal = (decimal)2.7182818284590452353602874713526624977572470;
+    }
+}
