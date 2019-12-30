9d37a88e3fb4602fb39735130dbd37c61fcd7a66
RedlMark <33761903+MARKOCA2006@users.noreply.github.com>
2019-12-30 22:44:33 +0100

    Update MathSharp.cs

--- MathSharp.cs
+++ MathSharp.cs
@@ -2,47 +2,27 @@ namespace MathSharp
 {
     public static class Consts
     {
-        //CONSTS
-        
-        // PI
-
-        // Returns the value of PI as a float
         public static float PiAsFloat = (float)3.141592653589793238462643383279502884197169399375105820974944592307816406;
-        // Returns the value of PI as a double
         public static double PiAsDouble = (double)3.141592653589793238462643383279502884197169399375105820974944592307816406;
-        // Returns the value of PI as a decimal
         public static decimal PiAsDecimal = (decimal)3.141592653589793238462643383279502884197169399375105820974944592307816406;
-
-        // e
-
-        // Returns the value of E as a float
+        
         public static float eAsFloat = (float)2.7182818284590452353602874713526624977572470;
-        // Returns the value of E as a float
         public static double eAsDouble = (double)2.7182818284590452353602874713526624977572470;
-        // Returns the value of E as a decimal
         public static decimal eAsDecimal = (decimal)2.7182818284590452353602874713526624977572470;
 
-        // Pythagoras' constant
-
-        // Returns the value of Pythagoras' constant as a float
         public static float PytagorasAsFloat = (float)1.41421356237309504880168872420969807856967187537694807317667973799;
-        // Returns the value of Pythagoras' constant as a float
         public static double PytagorasAsDouble = (double)1.41421356237309504880168872420969807856967187537694807317667973799;
-        // Returns the value of Pythagoras' constant as a float
         public static decimal PytagorasAsDecimal = (decimal)1.41421356237309504880168872420969807856967187537694807317667973799;
     }
 
     
     public static class Infinity
     {
-        // Returns the value of positive infinity
         public static double posInfinity = double.PositiveInfinity;
-        // Returns the value of positive infinity
         public static double negInfinity = double.NegativeInfinity;
     }
 
 
-    // Greek Alphabet
     public enum Greek
     {
         alpha, // 0
@@ -85,26 +65,35 @@ namespace MathSharp
         public static double Abs(double num)
         {
             if (num < 0)
-            {
                 return -num;
-            }
             else
-            {
                 return num;
-            }
+        }
+        
+        public static bool IsPrime(int num)
+        {
+            if (num == 2 || num == 3 || num == 5 || num == 7)
+                return true;
+            else if (num % 2 == 0)
+                return false;
+            else if (num % 3 == 0)
+                return false;
+            else if (num % 5 == 0)
+                return false;
+            else if (num % 7 == 0)
+                return false;
+            else
+                return true;
         }
     }
 
 
     public class Algebra
     {
-        // Factorial
         public static int Factorial(int num)
         {
             if (num <= 0)
-            {
                 return 1;
-            }
             return Factorial(num - 1) * num;
         }
     }
